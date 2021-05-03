65319cd96b4ab1df01a2e4a166d14a35f3e82ec5
Clea Parcerisas <cleap@vliz.be>
2021-05-03 11:03:04 +0200

    merge third octave bands only the values

--- soundexplorer/dataset.py
+++ soundexplorer/dataset.py
@@ -430,7 +430,7 @@ class Deployment:
             evo = asa.evolution_multiple(method_list=features, band_list=band_list)
             if third_octaves is not False:
                 evo_freq = asa.evolution_freq_dom('third_octaves_levels', band=third_octaves, db=True)
-                evo = evo.merge(evo_freq, left_index=True, right_index=True)
+                evo = evo.merge(evo_freq['oct3'], left_index=True, right_index=True)
         else:
             if third_octaves is not False:
                 evo = asa.evolution_freq_dom('third_octaves_levels', band=third_octaves, db=True)
